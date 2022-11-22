ebbad6550d6ac3595057574c7efef24deb817bab
Wai_Yan_Htet <waiyanhtet1661390@gmail.com>
2022-11-22 13:05:16 +0630

    crate moviecard component

--- src/components/moviecard/moviecard.component.jsx
+++ src/components/moviecard/moviecard.component.jsx
@@ -1,8 +1,14 @@
 import "./movicard.css";
 import React from "react";
+import ButtonC from "../buttonC/buttonC.component";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ image, buttonC }) => {
+  return (
+    <div className="movie-card group">
+      <img src={image} className="movie-card--image" alt="" />
+      <ButtonC {...buttonC} />
+    </div>
+  );
 };
 
 export default MovieCard;
--- src/home.page.jsx
+++ src/home.page.jsx
@@ -3,15 +3,54 @@ import React, { useState } from "react";
 import FlexContainer from "./components/FlexContainer/flexContainer.component";
 import Search from "./components/search/search.component";
 import ButtonC from "./components/buttonC/buttonC.component";
+import MovieCard from "./components/moviecard/moviecard.component";
 
 const HomePage = () => {
   const [searchValue, setSearchValue] = useState("");
+
+  const addFav = (id, image) => {};
 
   return (
     <>
       <Search search={searchValue} setSearch={setSearchValue} />
 
-      <FlexContainer scrolling={true} gap="sm"></FlexContainer>
+      <FlexContainer scrolling={true} gap="sm">
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          buttonC={{
+            onClick: () => addFav("1", "dsfsdf"),
+            children: "Add to Fav ❤️",
+          }}
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          buttonC={{
+            onClick: () => addFav("1", "dsfsdf"),
+            children: "Add to Fav ❤️",
+          }}
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          buttonC={{
+            onClick: () => addFav("1", "dsfsdf"),
+            children: "Add to Fav ❤️",
+          }}
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          buttonC={{
+            onClick: () => addFav("1", "dsfsdf"),
+            children: "Add to Fav ❤️",
+          }}
+        />
+        <MovieCard
+          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+          buttonC={{
+            onClick: () => addFav("1", "dsfsdf"),
+            children: "Add to Fav ❤️",
+          }}
+        />
+      </FlexContainer>
     </>
   );
 };
