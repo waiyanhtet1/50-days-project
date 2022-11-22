33aebf395cb74f45e4451d8780529c55118c5fd7
Wai_Yan_Htet <waiyanhtet1661390@gmail.com>
2022-11-22 13:45:46 +0630

    create homepage

--- src/home.page.jsx
+++ src/home.page.jsx
@@ -1,55 +1,82 @@
 import "./home.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FlexContainer from "./components/FlexContainer/flexContainer.component";
 import Search from "./components/search/search.component";
 import ButtonC from "./components/buttonC/buttonC.component";
 import MovieCard from "./components/moviecard/moviecard.component";
 
 const HomePage = () => {
+  const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [favorites, setFavorites] = useState([]);
 
-  const addFav = (id, image) => {};
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=73e90078`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    const favouriteMovies = JSON.parse(
+      localStorage.getItem("favourite-movie-list")
+    );
+    setFavorites(favouriteMovies);
+  }, []);
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  const addFavHandler = (id, image) => {
+    const movieList = [...favorites, { id, image }];
+    setFavorites(movieList);
+    localStorage.setItem("favourite-movie-list", JSON.stringify(movieList));
+  };
+
+  const removeFavHandler = (id) => {
+    const removedMovies = favorites.filter((fav) => fav.id !== id);
+    setFavorites(removedMovies);
+    localStorage.setItem("favourite-movie-list", JSON.stringify(removedMovies));
+  };
 
   return (
     <>
-      <Search search={searchValue} setSearch={setSearchValue} />
+      <FlexContainer justify="spaceBetween">
+        <h1>Movies</h1>
+        <Search search={searchValue} setSearch={setSearchValue} />
+      </FlexContainer>
 
       <FlexContainer scrolling={true} gap="sm">
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          buttonC={{
-            onClick: () => addFav("1", "dsfsdf"),
-            children: "Add to Fav ❤️",
-          }}
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          buttonC={{
-            onClick: () => addFav("1", "dsfsdf"),
-            children: "Add to Fav ❤️",
-          }}
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          buttonC={{
-            onClick: () => addFav("1", "dsfsdf"),
-            children: "Add to Fav ❤️",
-          }}
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          buttonC={{
-            onClick: () => addFav("1", "dsfsdf"),
-            children: "Add to Fav ❤️",
-          }}
-        />
-        <MovieCard
-          image="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-          buttonC={{
-            onClick: () => addFav("1", "dsfsdf"),
-            children: "Add to Fav ❤️",
-          }}
-        />
+        {movies.map((movie, i) => (
+          <MovieCard
+            key={i}
+            image={movie.Poster}
+            buttonC={{
+              onClick: () => addFavHandler(movie.imdbID, movie.Poster),
+              children: "Add to Fav ❤️",
+            }}
+          />
+        ))}
+      </FlexContainer>
+
+      <FlexContainer>
+        <h1>Favourites</h1>
+      </FlexContainer>
+
+      <FlexContainer scrolling={true} gap="sm">
+        {favorites.map((fav, i) => (
+          <MovieCard
+            key={i}
+            image={fav.image}
+            buttonC={{
+              onClick: () => removeFavHandler(fav.id),
+              children: "Remove to Fav ❌",
+            }}
+          />
+        ))}
       </FlexContainer>
     </>
   );
